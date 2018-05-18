1ee2ba255b5dea4195539e6387d4e4526eddf99c
sanchitaittech <39270684+sanchitaittech@users.noreply.github.com>
2018-05-18 10:47:28 +1000

    Update wp-config-sample.php

--- roles/db/wp-config-sample.php
+++ roles/db/wp-config-sample.php
@@ -20,16 +20,16 @@
 
 // ** MySQL settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define('DB_NAME', 'database_name_here');
+define('DB_NAME', '{{ db_name }}');
 
 /** MySQL database username */
-define('DB_USER', 'username_here');
+define('DB_USER', '{{ db_user }}');
 
 /** MySQL database password */
-define('DB_PASSWORD', 'password_here');
+define('DB_PASSWORD', '{{ db_password }}');
 
 /** MySQL hostname */
-define('DB_HOST', 'localhost');
+define('DB_HOST', '{{ db_server }}');
 
 /** Database Charset to use in creating database tables. */
 define('DB_CHARSET', 'utf8');
